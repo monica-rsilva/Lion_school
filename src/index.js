7f45db6e680ef895c56e9315aece26cae1ae4224
MÔNICA RIBEIRO DA SILVA <22282167@CFP127ALUNO.LOCAL>
2023-04-06 11:29:23 -0300

    nova parte inserida

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const criarButton = cursos => {
 
   const button = document.createElement('button')
   button.classList.add('buttonCurso')
+  button.id = cursos.sigla
 
   const icone = document.createElement('img')
   icone.src = cursos.icone
@@ -15,10 +16,20 @@ const criarButton = cursos => {
 
   button.append(icone,name)
 
+  button.addEventListener('click', function(){
+
+    localStorage.setItem('id',button.id)
+
+   carregarCardAluno()
+
+  })
+
   return button
 }
+
 const carregarCursos = async() => {
-  const url = `https://api-lion-school.onrender.com/v1/lion-school/cursos`;
+
+  const url = `https://segredo.onrender.com/v1/lion-school/cursos`;
 
   const response = await fetch(url);
   const data = await response.json();
@@ -28,7 +39,9 @@ const carregarCursos = async() => {
   const cardButton = cursos.map(criarButton)
 
   container.replaceChildren(...cardButton)
+
 }
+
 carregarCursos();
 
 const cardAluno = alunos => {
@@ -37,12 +50,54 @@ const cardAluno = alunos => {
   card.classList.add('card_aluno')
 
   const img = document.createElement('img')
-  img.src = alunos.foto
+  img.src = alunos.photo
 
   const name = document.createElement('span')
-  name.textContent = alunos.nome
+  name.textContent = alunos.name
 
-  
+  card.append(img,name)
+
+  return card
 
 }
+
+  const carregarCardAluno = async() => {
+
+    let curso = localStorage.getItem('id')
+
+    const url = `https://segredo.onrender.com/v1/lion-school/alunos/por/${curso}`
+
+    const response = await fetch(url);
+    const data = await response.json();
+    const alunos = await data.student
+    
+    const container = document.querySelector('#main')
+
+    const header = document.querySelector('#main header')
+    container.removeChild(header);
+
+    const cards = document.createElement('div')
+    cards.classList.add('div_cards')
+
+    const noasme = document.createElement('div')
+    noasme.classList.add('card_aluno')
+
+    const img = document.createElement('img')
+    img.src = alunos.photo
+
+    const nome = document.createElement('span')
+    nome.textContent = alunos.name
+
+    // noasme.appendchildren(img,nome)
+
+    cards.append(noasme)
+
+    container.appendChild(cards)
+
+    const card_Aluno = alunos.map(cardAluno)
+
+    cards.replaceChildren(...card_Aluno)
+
+  }
+  
 
